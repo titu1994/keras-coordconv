f1eada74e98bac6ceb3f1eb42111189422147837
Simon Brugman <sbrugman@users.noreply.github.com>
2018-07-13 11:31:17 +0200

    Update make_dataset.py
    Changes to make it work on my machine, not sure if compatible.

--- experiments/make_dataset.py
+++ experiments/make_dataset.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     tf.set_random_seed(0)
 
     # From https://arxiv.org/pdf/1807.03247.pdf
-    onehots = np.pad(np.eye(3136).reshape((3136, 56, 56, 1)),
+    onehots = np.pad(np.eye(3136, dtype='float32').reshape((3136, 56, 56, 1)),
                      ((0, 0), (4, 4), (4, 4), (0, 0)), mode="constant")
 
     images = tf.nn.conv2d(onehots, np.ones((9, 9, 1, 1)), [1] * 4, "SAME")
@@ -50,8 +50,8 @@ if __name__ == '__main__':
     else:
         # Create the quadrant datasets
         pos = np.where(onehots == 1.0)
-        X = pos[1]
-        Y = pos[2]
+        X = pos[0]
+        Y = pos[1]
 
         train_set = []
         test_set = []
